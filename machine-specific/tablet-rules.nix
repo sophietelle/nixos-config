0f9216a0db320859d18a51d7ae5aa8d076cdb831
Sophie <ryuko@inbox.lv>
2026-01-13 20:00:43 +0200

    Add tablet settings

--- machine-specific/tablet-rules.nix
+++ machine-specific/tablet-rules.nix
@@ -4,6 +4,18 @@
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
 
-  services.udev.packages = [ pkgs.opentabletdriver ];
-  # hardware.opentabletdriver.enable = true;
+  # Wacom CTL-472
+  # - Rotation: 180
+  # - Area position:
+  #   - X: 114 mm
+  #   - Y: 51 mm
+  # - Area dimensions:
+  #   - Width: 75 mm
+  #   - Height: 52.5 mm
+  # - Filters:
+  #   - BezierInterpolator:
+  #     - Pre-interp smoothing: 3
+  #     - Tilt smoothing: 1
+  #     - Frequency: 200hz
+  hardware.opentabletdriver.enable = true;
 }
