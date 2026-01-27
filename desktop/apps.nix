f53b8e875e958b4f4f166e58b68d4a303a5a05de
Sophie <ryuko@inbox.lv>
2026-01-27 20:12:45 +0200

    apps.nix: restructure

--- desktop/apps.nix
+++ desktop/apps.nix
@@ -5,11 +5,10 @@
     enable = true;
   };
 
-  # For trash folder in explorer
-  services.gvfs.enable = true;
-
-  # For thumbnails in Thunar
-  services.tumbler.enable = true;
+  # Thunar thingies
+  services.gvfs.enable = true; # Trash folder
+  services.tumbler.enable = true; # Thumbnails
+  programs.xfconf.enable = true; # Explorer preferences
 
   # CLI tools that will be available for any user.
   environment.systemPackages = with pkgs; [
