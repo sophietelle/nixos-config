a74daec1f6fe0b4b45d64d7e41e3b9bcddc97a95
Sophie <ryuko@inbox.lv>
2026-02-17 07:24:07 +0200

    rework sound latency for osu

--- home/osu.nix
+++ home/osu.nix
@@ -1,6 +1,30 @@
 # Sophie: https://git.pompy.dev/pomp/.dotfiles/src/commit/86717a4b621e6aa377a3cf68e69344acdd986984/modules/home-manager/osu.nix
-{ pkgs, inputs, ... }:
+# Lazer runs at 44100hz. libbass is a bitch and i hate it sincerely
+# Make sure to set 44100hz system-wide default sample rate to avoid resampling (-latency, +sound quality)
+{ pkgs, inputs, lib, ... }:
 
+let
+  # Adapt if sound corruption happens
+  # Good values: 32, 64, 96, 128
+  latency = "64";
+  rate = "44100";
+
+  osuEnv = [
+    # https://github.com/PipeWire/pipewire?tab=readme-ov-file#usage
+    "PIPEWIRE_LATENCY=${latency}"
+    "PIPEWIRE_QUANTUM=${latency}"
+    "PIPEWIRE_RATE=${rate}"
+    "PIPEWIRE_NODE=\"{node.latency=${latency}/${rate}}\""
+    # https://download.nvidia.com/XFree86/Linux-x86_64/525.78.01/README/openglenvvariables.html
+    "__GL_MaxFramesAllowed=1"
+    "__GL_SYNC_TO_VBLANK=0"
+    "vblank_mode=0"
+
+    "obs-gamecapture"
+    # programs.gamemode.enable in nix system config
+    "gamemoderun"
+  ];
+in
 {
   home.packages = with pkgs; [
     osu-lazer-bin
@@ -10,9 +34,7 @@
   home.file.".local/share/applications/osu.desktop".text = ''
     [Desktop Entry]
     Name=osu! (custom)
-    # https://download.nvidia.com/XFree86/Linux-x86_64/525.78.01/README/openglenvvariables.html
-    # https://github.com/PipeWire/pipewire?tab=readme-ov-file#usage
-    Exec=env PIPEWIRE_LATENCY=64/44100 __GL_MaxFramesAllowed=1 vblank_mode=0 __GL_SYNC_TO_VBLANK=0 obs-gamecapture gamemoderun osu! %U
+    Exec=env ${lib.concatStringsSep " " osuEnv} osu! %U
     Categories=Game
     Comment=A free-to-win rhythm game. Rhythm is just a *click* away!
     SingleMainWindow=true
--- machine-specific/sound-latency.nix
+++ machine-specific/sound-latency.nix
@@ -1,18 +1,4 @@
-# Sophie: Adapted from https://github.com/fufexan/nix-gaming, an MIT-licensed project
-# https://github.com/fufexan/nix-gaming/blob/568772891364aaa7e29da6dd4f0b2f626f3a62ae/modules/pipewireLowLatency.nix
-
-let
-  inputMatch = "Family 17h/19h/1ah HD Audio Controller"; # wpctl status + wpctl inspect <id>
-
-  quantum = 64;
-  rate = 44100;
-  format = "S32LE";
-
-  qr = "${toString quantum}/${toString rate}";
-in
 {
-  powerManagement.cpuFreqGovernor = "performance";
-
   boot.kernelParams = [
     "threadirqs"
     "preempt=full"
@@ -46,76 +32,13 @@ in
     }
   ];
 
+  # For osu!lazer.
   services.pipewire = {
     extraConfig = {
       pipewire."99-lowlatency" = {
         "context.properties" = {
-          "default.clock.rate" = rate;
-          "cpu.zero.denormals" = true;
-          "mem.allow-mlock" = true;
-          "mem.mlock-all" = true;
-          "link.max-buffers" = 16;
+          "default.clock.rate" = 44100;
         };
-
-        "context.properties.rules" = [{
-          matches = [{
-            "device.description" = inputMatch;
-          }];
-          actions = {
-            update-props = {
-              "default.clock.min-quantum" = quantum;
-              "default.clock.quantum" = quantum;
-              "default.clock.max-quantum" = quantum;
-            };
-          };
-        }];
-
-        "context.modules" = [
-          {
-            name = "libpipewire-module-rt";
-            flags = [
-              "ifexists"
-              "nofail"
-            ];
-            args = {
-              "nice.level" = -20;
-              "rt.prio" = 99;
-              "rt.time.soft" = 200000;
-              "rt.time.hard" = 200000;
-            };
-          }
-        ];
-      };
-
-      pipewire-pulse."99-lowlatency"."pulse.properties" = {
-        "server.address" = ["unix:native"];
-        "pulse.min.req" = qr;
-        "pulse.min.quantum" = qr;
-        "pulse.min.frag" = qr;
-      };
-
-      client."99-lowlatency"."stream.properties" = {
-        "node.latency" = qr;
-        "resample.quality" = 1;
-      };
-    };
-
-    wireplumber = {
-      extraConfig = {
-        "99-alsa-lowlatency"."monitor.alsa.rules" = [
-          {
-            matches = [{"node.name" = "~alsa_output.*";}];
-            actions.update-props = {
-              "audio.format" = format;
-              "audio.rate" = rate;
-              "api.alsa.period-size" = quantum;
-              "api.alsa.headroom" = quantum;
-              # "api.alsa.headroom" = quantum;
-              # "api.alsa.period-num" = 3;
-              "session.suspend-timeout-seconds" = 0;
-            };
-          }
-        ];
       };
     };
   };
