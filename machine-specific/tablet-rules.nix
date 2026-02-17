e3a4c6868069967c4903967fa96b8c2c15d4ea41
Sophie <ryuko@inbox.lv>
2026-02-17 07:24:54 +0200

    remove tablet settings comment

--- machine-specific/tablet-rules.nix
+++ machine-specific/tablet-rules.nix
@@ -4,18 +4,5 @@
   hardware.uinput.enable = true;
   boot.kernelModules = [ "uinput" ];
 
-  # Wacom CTL-472
-  # - Rotation: 180
-  # - Area position:
-  #   - X: 114 mm
-  #   - Y: 51 mm
-  # - Area dimensions:
-  #   - Width: 75 mm
-  #   - Height: 52.5 mm
-  # - Filters:
-  #   - BezierInterpolator:
-  #     - Pre-interp smoothing: 3
-  #     - Tilt smoothing: 1
-  #     - Frequency: 200hz
   hardware.opentabletdriver.enable = true;
 }
